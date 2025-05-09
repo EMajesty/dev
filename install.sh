2b313a3915bd1513b5cdf1962b509af8b6e412cf
EMajesty <lassi.laitinen@protonmail.com>
2025-05-09 20:40:32 +0300

    test aur pkg install with script

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 echo "Hell World"
+
+AUR_PACKAGES=(
+    discord-canary
+)
+
+for pkg in "${AUR_PACKAGES[@]}" do
+    git clone "https://aur.archlinux.org/${pkg}.git"
+    cd "$pkg"
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf "$pkg"
+done
